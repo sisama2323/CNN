7632b5e952426a16b121643899fbabefca65017f
Siwei Guo <sisama2323@gmail.com>
2018-02-09 23:20:19 -0800

    Add GoogLeNet

--- net.py
+++ net.py
@@ -144,59 +144,96 @@ class Net_2(nn.Module):
         return x
 
 # model 3
-class Net_3(nn.Module):
-    def __init__(self, n_cov1=6, n_cov2=16, n_cov3=32, n_kernel1=3, n_kernel2=3, n_kernel3=3, n_hid1=64, n_hid2=128):
-        super(Net_2, self).__init__()
-        self.n_cov1 = n_cov1
-        self.n_cov2 = n_cov2
-        self.n_cov3 = n_cov3
-        self.n_kernel1 = n_kernel1
-        self.n_kernel2 = n_kernel2
-        self.n_kernel3 = n_kernel3
-        self.n_hid1 = n_hid1
-        self.n_hid2 = n_hid2
-        self.feature = numfeature([n_kernel1, n_kernel2, n_kernel3])
+class Inception(nn.Module):
+    '''
+    Code reference: https://github.com/kuangliu/pytorch-cifar/blob/master/models/googlenet.py
+    Paper referece: https://arxiv.org/pdf/1409.4842.pdf
+    '''
+    def __init__(self, n_input, n_b1=6, n1_b2=3, n2_b2=6, n1_b3=3, n2_b3=6, n1_b4=6):
+        super(Inception, self).__init__()
+    
+        # first branch
+        self.b1 = nn.Sequential(
+            nn.Conv2d(n_input, n_b1, kernel_size=1, stride=1),
+            nn.BatchNorm2d(n_b1),
+            nn.ReLU(True)
+        )
 
-        self.pool = nn.MaxPool2d(2, 2)
+        # 2nd branch
+        self.b2 = nn.Sequential(
+            nn.Conv2d(n_input, n1_b2, kernel_size=1, stride=1),
+            nn.BatchNorm2d(n1_b2),
+            nn.ReLU(True),
+            nn.Conv2d(n1_b2, n2_b2, kernel_size=3, stride=1),
+            nn.BatchNorm2d(n2_b2),
+            nn.ReLU(True)
+        )
 
-        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
-        self.conv1 = nn.Conv2d(3, self.n_cov1, self.n_kernel1, stride=1, padding=1)
-        nn.init.xavier_uniform(self.conv1.weight)
-        self.conv1_bn = nn.BatchNorm2d(self.n_cov1)
+        # 3rd branch
+        self.b3 = nn.Sequential(
+            nn.Conv2d(n_input, n1_b3, kernel_size=1, stride=1),
+            nn.BatchNorm2d(n1_b3),
+            nn.ReLU(True),
+            nn.Conv2d(n1_b3, n2_b3, kernel_size=5, stride=1),
+            nn.ReLU(True)
+        )
+
+        # 4th branch
+        self.b4 = nn.Sequential(
+            nn.MaxPool2d(3, stride=1, padding=1),
+            nn.Conv2d(n_input, n1_b4, kernel_size=1, stride=1),
+            nn.BatchNorm2d(n1_b4),
+            nn.ReLU(True)
+        )
         
-        self.conv2 = nn.Conv2d(self.n_cov1, self.n_cov2, self.n_kernel2, stride=1, padding=1)
-        nn.init.xavier_uniform(self.conv2.weight)
-        self.conv2_bn = nn.BatchNorm2d(self.n_cov2)
+    def forward(self, x):
+        x1 = self.b1(x)
+        x2 = self.b2(x)
+        x3 = self.b3(x)
+        x4 = self.b4(x)
+        return torch.cat([x1,x2,x3,x4], 1)
 
-        self.conv3 = nn.Conv2d(self.n_cov2, self.n_cov3, self.n_kernel3, stride=1, padding=1)
-        nn.init.xavier_uniform(self.conv3.weight)
-        self.conv3_bn = nn.BatchNorm2d(self.n_cov3)
+class GoogLeNet(nn.Module):
+    '''
+    Code reference: https://github.com/kuangliu/pytorch-cifar/blob/master/models/googlenet.py
+    Paper referece: https://arxiv.org/pdf/1409.4842.pdf
+    '''
+    def __init__(self):
+        super(GoogLeNet, self).__init__()
 
+        self.a1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.MaxPool2d(3, stride=1, padding=1),
+            nn.Conv2d(64, 192, kernel_size=1),
+            nn.BatchNorm2d(192),
+            nn.ReLU(True)
+            nn.MaxPool2d(3, stride=1, padding=1)
+        )
+
+        self.a2 = Inception(n_input=192, n_b1=64, n1_b2=96, n2_b2=128, n1_b3=16, n2_b3=32, n1_b4=32)
+        self.a3 = Inception(256, 128, 128, 192, 32, 96, 64)
+
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a4 = Inception(480, 192,  96, 208, 16,  48,  64)
+        self.a5 = Inception(512, 160, 112, 224, 24,  64,  64)
+        self.a6 = Inception(512, 128, 128, 256, 24,  64,  64)
         
-        # torch.nn.Linear(in_features, out_features, bias=True)
-        self.fc1 = nn.Linear(self.feature * self.feature * self.n_cov3, self.n_hid1)
-        nn.init.xavier_uniform(self.fc1.weight)
-        self.fc1_bn = nn.BatchNorm2d(self.n_hid1)
+        self.avgpool = nn.AvgPool2d(8, stride=1)
 
-        self.fc2 = nn.Linear(self.n_hid1, self.n_hid2)
-        nn.init.xavier_uniform(self.fc2.weight)
-        self.fc2_bn = nn.BatchNorm2d(self.n_hid2)
-
-        self.fc3 = nn.Linear(self.n_hid2, 10)
-        nn.init.xavier_uniform(self.fc2.weight)
+        self.fc = nn.Linear(512, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1_bn(self.conv1(x))))
-        x = self.pool(F.relu(self.conv2_bn(self.conv2(x))))
-        x = self.pool(F.relu(self.conv3_bn(self.conv3(x))))
-
-        # leak relu
-        # x = self.pool(F.leaky_relu(self.conv1_bn(self.conv1(x))))
-        # x = self.pool(F.leaky_relu(self.conv2_bn(self.conv2(x))))
-        # x = self.pool(F.leaky_relu(self.conv3_bn(self.conv3(x))))
-
-        x = x.view(-1, self.feature * self.feature * self.n_cov3)
-        x = F.relu(self.fc1_bn(self.fc1(x)))
-        x = F.relu(self.fc2_bn(self.fc2(x)))
-        x = self.fc3(x)
+        x = self.a1(x)
+        x = self.a2(x)
+        x = self.a3(x)
+        x = self.maxpool(x)
+        x = self.a4(x)
+        x = self.a5(x)
+        x = self.a6(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
         return x
